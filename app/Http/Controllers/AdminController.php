b649af9a5a23049876deac51f0acd41bb646c966
Sachiko Sakamoto <sas19.gaoo@gmail.com>
2026-07-15 21:21:26 +0900

    管理画面の検索機能を実装

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,9 +9,30 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $contacts = Contact::with(['category', 'tags'])->paginate(7);
+        $contacts = Contact::with(['category', 'tags'])
+            ->when($request->keyword, function ($query) use ($request) {
+                $keyword = $request->keyword;
+
+                $query->where(function ($query) use ($keyword) {
+                    $query->where('first_name', 'like', "%{$keyword}%")
+                        ->orWhere('last_name', 'like', "%{$keyword}%")
+                        ->orWhere('email', 'like', "%{$keyword}%");
+                });
+            })
+            ->when($request->gender, function ($query) use ($request) {
+                $query->where('gender', $request->gender);
+            })
+            ->when($request->category_id, function ($query) use ($request) {
+                $query->where('category_id', $request->category_id);
+            })
+            ->when($request->date, function ($query) use ($request) {
+                $query->whereDate('created_at', $request->date);
+            })
+            ->paginate(7);
+
+
         $categories = Category::all();
         $tags = Tag::all();
 
